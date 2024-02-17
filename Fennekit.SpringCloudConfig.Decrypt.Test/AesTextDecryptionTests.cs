bf1d1791904a72eef739560b8619592e64e14bf5
erikk <erik.konijnenburg@fennekit>
2024-02-17 11:10:53 +0100

    Verify that no two encryptions are equal

--- Fennekit.SpringCloudConfig.Decrypt.Test/AesTextDecryptionTests.cs
+++ Fennekit.SpringCloudConfig.Decrypt.Test/AesTextDecryptionTests.cs
@@ -6,7 +6,7 @@ public class AesTextDecryptionTests
 {
     [Test]
     [TestCaseSource(nameof(GetTestVector))]
-    public void DecodeTestForSpringConfigCipher(string salt, string key, string plainText)
+    public void EncryptionShouldBeDecryptable(string salt, string key, string plainText)
     {
         var textDecryptor = new AesTextDecryptor(key, salt);
         var cipher = textDecryptor.Encrypt(plainText);
@@ -14,6 +14,18 @@ public class AesTextDecryptionTests
         Assert.That(decrypted, Is.EqualTo(plainText));
     }
 
+    [Test]
+    [TestCaseSource(nameof(GetTestVector))]
+    public void EncryptionShouldAlwaysBeDifferent(string salt, string key, string plainText)
+    {
+        var decryptor = new AesTextDecryptor(key, salt);
+          
+        var encrypt1 = decryptor.Encrypt(plainText);
+        var encrypt2 = decryptor.Encrypt(plainText);
+        
+        Assert.That(encrypt1, Is.Not.EqualTo(encrypt2));
+    }
+    
     private static IEnumerable<object[]> GetTestVector()
     {
         yield return
--- Fennekit.SpringCloudConfig.Decrypt.Test/RsaKeyStoreEncryptionTest.cs
+++ Fennekit.SpringCloudConfig.Decrypt.Test/RsaKeyStoreEncryptionTest.cs
@@ -7,7 +7,7 @@ public class RsaKeyStoreEncryptionTest
 
     [Test]
     [TestCaseSource(nameof(GetTestVector))]
-    public void DecodeTestForSpringConfigCipher(string salt, string strong, string algorithm, string plainText)
+    public void EncryptionShouldBeDecryptable(string salt, string strong, string algorithm, string plainText)
     {
         var decryptor = new RsaKeyStoreDecryptor("server.jks", "letmein", "mytestkey", salt: salt,
             strong: bool.Parse(strong), algorithm: algorithm);
@@ -16,6 +16,19 @@ public class RsaKeyStoreEncryptionTest
         var decrypted = decryptor.Decrypt(encrypt);
 
         Assert.That(decrypted, Is.EqualTo(plainText));
+    }
+    
+    [Test]
+    [TestCaseSource(nameof(GetTestVector))]
+    public void EncryptionShouldAlwaysBeDifferent(string salt, string strong, string algorithm, string plainText)
+    {
+        var decryptor = new RsaKeyStoreDecryptor("server.jks", "letmein", "mytestkey", salt: salt,
+            strong: bool.Parse(strong), algorithm: algorithm);
+
+        var encrypt1 = decryptor.Encrypt(plainText);
+        var encrypt2 = decryptor.Encrypt(plainText);
+        
+        Assert.That(encrypt1, Is.Not.EqualTo(encrypt2));
     }
 
     private static IEnumerable<object[]> GetTestVector()
